b25f2b4434e3fe1ee99926b9038e67d872323388
David Olah <david.olah@cofinpro.de>
2018-11-23 11:23:27 +0100

    Added Michael

---
+++ reacttest/src/NeuesElement.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NeuesElement extends Component {
+    render() {
+        let style = {
+            fontSize: '10em',
+            color: '#f00'
+        };
+        return (<div style={style}>Hi Michael</div>);
+    }
+}
+
+export default NeuesElement;
