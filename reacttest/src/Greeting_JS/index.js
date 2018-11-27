88af752cdce9da1a1dfe22941b65f6262642f6be
Akcay <EE0371D@UNION-INVESTMENT.DE>
2018-11-27 09:36:42 +0100

    Created JS for inputFIeld and Button

--- reacttest/src/Greeting_JS/index.js
+++ reacttest/src/Greeting_JS/index.js
@@ -1,42 +1,44 @@
-// window.onload=function(){
 
-// }
-// window.addEventListener("click",() => {
+function InputField(event) {
 
-// });
-function InputField() {
-
-    // var input = document.getElementById("nameInput").value;
-    // var inputColor = input.addEventListener("change", Inputfield);
-    // const greeting = "";
-    // // wenn Input eingabe > 0
-    // if (input === null) {
-    //     input.style.backgroundColor = "#FFCDD2";
-
-    // }
-    // // wenn Input eingabe <= 0
-    // else {
-    //     input.style.backgroundColor = "#aac4f1";
-    // }
-    // document.getElementById("nameInput").style.background = "#aac4f1";
-    var change = false;
-    var input = document.getElementById('nameInput');
-    input.addEventListener("change", () => {
-        input.onchange = function () {
-            if (!change) {
-
-                input.style.background = "#aac4f1";
-                change = true;
-            }
-            else {
-                input.style.background = "#FFCDD2";
-            }
-        }
-    });
-      
+    var input = document.getElementById("nameInput");
+    console.log(event.key + "=>" + String.fromCharCode(event.keyCode));
+    if (input.value == "") {
+        input.style.backgroundColor = "#aac4f1";
+    }
+    else {
+        input.style.backgroundColor = "#FFCDD2";
+    }
 }
+
 function ButtonSend() {
-    const greeting = "";
-    alert("Hallo, ", { greeting });
+        var input = document.getElementById("nameInput").value;
+        alert(`Hallo, ${input}`);
 }
+//  AUsgabe der noch übrigen erlaubten Zeichen=> zählt runter 
+function RemainingChars() {
+    // characterCount(document.getElementById('nameInput'), {
+    //     max:10,
+    //     output:document.getElementById('charsLeft'),
+    // })
+    // $(function(){
+    //     $('#nameInput').on("change", function(){
+    //         var count=$("input:text[value='']").length;
+    //         alert(count);
+    //     });
+    // });
+    // $(document).ready(function() {
+    //     var text_max = 10;
+    //     $('input').html(text_max + ' characters remaining');
+
+    //     $('input').on('keyup',function() {
+    //         var text_length = $('input').val().length;
+    //         var text_remaining = text_max - text_length;
+
+    //         $('input').html(text_remaining + ' characters remaining');
+    //     });
+    // });
+}
+
+
 
